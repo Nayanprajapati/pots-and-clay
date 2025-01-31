c8f97382fb5f4838c914c52662a4fa293b94a52a
Nayanprajapati <nayanpraz123@gmail.com>
2025-01-31 16:00:54 +0545

    success

--- src/Pages/Payment/Success.jsx
+++ src/Pages/Payment/Success.jsx
@@ -1,14 +1,14 @@
-import React from "react";
+// import React from "react";
 
-const Success = () => {
-  return (
-    <div className="container my-cart">
-      <p className="card-text">
-        Thank you for your purchase. Your payment was successful and your order
-        is being processed.
-      </p>
-    </div>
-  );
-};
+// const Success = () => {
+//   return (
+//     <div className="container my-cart">
+//       <p className="card-text">
+//         Thank you for your purchase. Your payment was successful and your order
+//         is being processed.
+//       </p>
+//     </div>
+//   );
+// };
 
-export default Success;
+// export default Success;
